92b7c31c11e98ccf18d46383f9d2c8f491e31063
Summer1121 <xtysummer1121@qq.com>
2019-01-21 20:28:10 +0800

    Update BaseMapper.java

--- Common/BaseMapper.java
+++ Common/BaseMapper.java
@@ -11,31 +11,31 @@ import org.apache.ibatis.annotations.UpdateProvider;
 public interface BaseMapper<T> {
 	// 增
 	@InsertProvider(type = BaseSqlProvider.class, method = "insert")
-	int save(@Param("object") T bean);
+	int save(T bean);
 
 	// 删
 	@DeleteProvider(type = BaseSqlProvider.class, method = "delete")
-	int delete(@Param("object")T bean);
+	int delete(T bean);
 
 	// 改(必须要有主键)
 	@UpdateProvider(type = BaseSqlProvider.class, method = "update")
-	int edit(@Param("object")T bean);
+	int edit(T bean);
 
 	// 查
 	// 一条数据
 	@ResultMap("BaseResultMap")
 	@SelectProvider(type = BaseSqlProvider.class, method = "selectOne")
-	T findOne(@Param("object")T bean);
+	T findOne(T bean);
 
 	// 一组数据
 	@ResultMap("BaseResultMap")
 	@SelectProvider(type = BaseSqlProvider.class, method = "selectList")
-	List<T> findList(@Param("object")T bean);
+	List<T> findList(T bean);
 
 	// 默认分页(pageNum=1,pageSize=10)
 	@ResultMap("BaseResultMap")
 	@SelectProvider(type = BaseSqlProvider.class, method = "selectPaginationAuto")
-	List<T> findListWithPageAuto(@Param("object")T bean);
+	List<T> findListWithPageAuto(T bean);
 
 	// 自定义分页
 	@ResultMap("BaseResultMap")
