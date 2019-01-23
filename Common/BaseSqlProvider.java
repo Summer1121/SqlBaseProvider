81bc997bc4816fa20a7803d0ed2177a6344aaea4
Summer1121 <1725469554@qq.com>
2019-01-23 10:09:29 +0800

    Add files via upload
    fix a bug with paging

--- Common/BaseSqlProvider.java
+++ Common/BaseSqlProvider.java
@@ -7,18 +7,18 @@
 * @version V1.0   
 */
 
-package com.ncepu.feilong505.AOSystem.Common;
+package com.ncepu.feilong505.OASystem.Common;
 
 import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
-
 import org.apache.ibatis.jdbc.SQL;
 import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.mapping.ResultMap;
 import org.apache.ibatis.mapping.ResultMapping;
+import org.springframework.web.servlet.mvc.method.annotation.RequestResponseBodyMethodProcessor;
 
 /**
  * @ClassName: BaseSqlProvider
@@ -50,22 +50,18 @@ public class BaseSqlProvider {
 		BaseSqlProvider.properties = properties;
 	}
 
-	
-	
 	// 初始化
 	private void init() {
-		//当存在多个mapper也就存在多个basesqlmapper，会“模棱两可”问题，
-		//需要指定BaseResultMap的完整路径
+		// 当存在多个mapper也就存在多个basesqlmapper，会“模棱两可”问题，
+		// 需要指定BaseResultMap的完整路径
 		String id = mappedStmt.getId();
-		if(id!=null)
-		{
-			int index=id.lastIndexOf(".");
-			if(index!=-1)
-			{
-				String prefix=id.substring(0, index);
-				String resultMapId=prefix+".BaseResultMap";
+		if (id != null) {
+			int index = id.lastIndexOf(".");
+			if (index != -1) {
+				String prefix = id.substring(0, index);
+				String resultMapId = prefix + ".BaseResultMap";
 				ResultMap resultMap = mappedStmt.getConfiguration().getResultMap(resultMapId);
-				//主键
+				// 主键
 				idResult = resultMap.getIdResultMappings().get(0);
 				parse(resultMap);
 			}
@@ -80,13 +76,13 @@ public class BaseSqlProvider {
 		}
 	}
 
-	//控制台输出sql
+	// 控制台输出sql
 	private void sqlDebug(SQL sql) {
 		System.out.println(sql.toString());
 		return;
 	}
 
-	//插入一个数据
+	// 插入一个数据
 	public String insert(Object object) {
 		if (object == null)
 			throw new RuntimeException("The target can't be Null.");
@@ -129,7 +125,7 @@ public class BaseSqlProvider {
 		}
 	}
 
-	//删除一个数据
+	// 删除一个数据
 	public String delete(Object object) {
 		if (object == null)
 			throw new RuntimeException("The target can't be Null.");
@@ -173,7 +169,7 @@ public class BaseSqlProvider {
 
 	}
 
-	//更新一个数据
+	// 更新一个数据
 	public String update(Object object) {
 		if (object == null)
 			throw new RuntimeException("The target can't be Null.");
@@ -228,7 +224,7 @@ public class BaseSqlProvider {
 		}
 	}
 
-	//查找一组数据
+	// 查找一组数据
 	public String selectList(Object object) {
 		if (object == null)
 			throw new RuntimeException("The target can't be Null.");
@@ -272,23 +268,61 @@ public class BaseSqlProvider {
 		}
 	}
 
-	//查找一条数据
+	// 查找一条数据
 	public String selectOne(Object object) {
 		return selectList(object);
 	}
 
-	//默认分页查询
+	// 默认分页查询
 	public String selectPaginationAuto(Object object) {
 		return selectPagination(object, 1, 10);
 	}
-	
-	//自定义的分页查询
+
+	// 自定义的分页查询
+	// 重写生成sql，因为object作为一个参数无法直接提供内部参数
 	public String selectPagination(Object object, int pageNum, int pageSize) {
-		String sqlOld = selectList(object);
-		StringBuilder sql=new StringBuilder(sqlOld.toString());
-		String limit=String.format(" LIMIT %s, %s", (pageNum-1)*pageSize, pageSize);
-		sql.append(limit);
-		System.out.println(limit);
-		return sql.toString();
+		if (object == null)
+			throw new RuntimeException("The target can't be Null.");
+		else {
+			String ans=null;
+			SQL sql = new SQL();
+			try {
+				// TABLE
+				Class<?> clazz = object.getClass();
+				Table table = (Table) clazz.getAnnotation(Table.class);
+				String tableName = table.value();
+				// SELECT
+				sql.SELECT("*");
+				sql.FROM(tableName);
+				// WHERE
+				for (Map.Entry<String, String> pair : baseResultMapping.entrySet()) {
+					String property = pair.getKey();
+					String column = pair.getValue();
+					Field field = clazz.getDeclaredField(property);
+					field.setAccessible(true);
+					Object value = field.get(object);
+					if (value != null) {
+						sql.WHERE(String.format("`%s`= #{object.%s}", column, property));
+					}
+				}
+				StringBuilder sqlNew = new StringBuilder(sql.toString());
+				sqlNew.append(String.format("LIMIT %s, %s", (pageNum - 1) * pageSize, pageSize));
+				ans = sqlNew.toString();
+				System.out.println(ans);
+			} catch (NoSuchFieldException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SecurityException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalArgumentException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return ans;
+		}
 	}
 }
