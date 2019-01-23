362f851143f1fd8630535fe65b534c3c747a4ef3
Summer1121 <1725469554@qq.com>
2019-01-23 10:34:24 +0800

    Add files via upload

---
+++ Common/ObjectMap.java
@@ -0,0 +1,53 @@
+package com.ncepu.feilong505.OASystem.Common;
+
+import java.beans.BeanInfo;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * TODO
+ * 
+ * @author xtysummer1121@foxmail.com
+ * @date 2019年1月22日
+ */
+public class ObjectMap {
+	public static Map<?, ?> objectToMap(Object obj) {
+		if (obj == null)
+			return null;
+		Map<String, Object> map = new HashMap<String, Object>();
+		try {
+			BeanInfo beanInfo = Introspector.getBeanInfo(obj.getClass());
+			PropertyDescriptor[] propertyDescriptors = beanInfo.getPropertyDescriptors();
+			for (PropertyDescriptor property : propertyDescriptors) {
+				String key = property.getName();
+
+				// 过滤class属性
+				if (!key.equals("class")) {
+					// 得到property对应的getter方法
+					Method getter = property.getReadMethod();
+					Object value = getter.invoke(obj);
+
+					map.put(key, value);
+				}
+
+			}
+		} catch (Exception e) {
+			System.out.println("transBean2Map Error " + e);
+		}
+		return map;
+	}
+
+	public static Object MapToObject(Map<String, Object> map, Class<?> beanClass)
+			throws InstantiationException, IllegalAccessException, InvocationTargetException {
+		if (map == null)
+			return null;
+		Object obj = beanClass.newInstance();
+		org.apache.commons.beanutils.BeanUtils.populate(obj, map);
+		return obj;
+	}
+
+}
